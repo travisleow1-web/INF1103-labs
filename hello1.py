267ebdaf88d1cfa6b426b152f0272708f45cb00d
Travis <travisleow1@gmail.com>
2026-09-04 17:14:25 +0800

    Second Post

---
+++ hello1.py
@@ -0,0 +1,25 @@
+#Activity 1
+print("================")
+print("Welcome Here")
+print("My first post!")
+print("================")
+
+#3A) The program does display the way it was written
+#3B) It follows a top to bottom approach, executing each line of code in the order it appears. The print statements are executed first, followed by the variable assignments and subsequent print statements that display the values of those variables. This sequential execution ensures that the output is generated in a logical and organized manner, reflecting the structure of the code as written.
+#3C) The output of the program is displayed in the console or terminal where the code is executed. The print statements generate text output that is visible to the user, allowing them to see the welcome message, user information, and any other relevant details as specified in the code.
+
+#Activity 2:
+print("================")
+print("Welcome Here")
+print("My first post!")
+print("================")
+
+username = "cool_creator"
+bio = "fun blogger"
+followers = 100
+
+print("Username: " ,username)
+print("Bio: " ,bio)
+print("Followers: " ,followers)
+#2A) The use of having variables in this code is to store information about the user, such as their username, bio, and number of followers. This allows for easy access and modification of these values throughout the program.
+#2B) If we are talking about the code stored inside the variables, when we change the value of the variables, the output when using said variables would change.
