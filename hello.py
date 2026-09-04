f0f5789ee47a7e3682e906d1c91d67bc3afdbcfa
Travis <travisleow1@gmail.com>
2026-09-04 16:45:21 +0800

    Second Post

--- hello.py
+++ hello.py
@@ -2,3 +2,14 @@ print("================")
 print("Welcome Here")
 print("My first post!")
 print("================")
+
+username = "cool_creator"
+bio = "fun blogger"
+followers = 100
+
+print("Username: " ,username)
+print("Bio: " ,bio)
+print("Followers: " ,followers)
+#2A) The use of having variables in this code is to store information about the user, such as their username, bio, and number of followers. This allows for easy access and modification of these values throughout the program.
+#2B) If we are talking about the code stored inside the variables, when we change the value of the variables, the output when using said variables would change.
+
